3eaa47a30dffb849246f9bc5208fcd1d5760599b
Yash Panpaliya <yashpanpaliya11@Yashs-MacBook-Pro-7.local>
2026-04-25 00:27:32 +0530

    search logic

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,8 +4,16 @@ import data from "../Data/app.json";
 import "./Navbar.css";
 
 export default function Navbar() {
+  const [query, setQuery] = useState("");
+
+  // filter from JSON
+  const filtered = data.apps.filter((item) =>
+    item.name.toLowerCase().includes(query.toLowerCase())
+  );
+
   return (
     <nav className="navbar">
+      {/* LEFT */}
       <div className="nav-left">
         <img
           src="https://upload.wikimedia.org/wikipedia/commons/4/44/Microsoft_logo.svg"
@@ -24,9 +32,35 @@ export default function Navbar() {
       <div className="nav-center">
         <input
           type="text"
-          placeholder="Search apps, games, and more"/>
-      </div>
+          placeholder="Search apps, games..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
 
+        {/* DROPDOWN */}
+        {query && (
+          <div className="search-box">
+            {filtered.length > 0 ? (
+              filtered.slice(0, 6).map((item) => (
+                <div key={item.id} className="search-item">
+                  
+                  {/* IMAGE */}
+                  <img src={item.image} alt="" />
+
+                  {/* TEXT */}
+                  <div>
+                    <p>{item.name}</p>
+                    <span>{item.category}</span>
+                  </div>
+
+                </div>
+              ))
+            ) : (
+              <div className="search-item">No results found</div>
+            )}
+          </div>
+        )}
+      </div>
 
       {/* RIGHT */}
       <div className="nav-right">
